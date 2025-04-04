ba98d11b34805924035b4c9b2da5c9c664f9e681
witoon-skydea <witoonp@gmail.com>
2025-04-04 15:27:38 +0700

    Removed deprecated MongoDB connection options

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -5,10 +5,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log('Connected to MongoDB Atlas');
     return true;
   } catch (error) {
